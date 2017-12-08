3f394a49075c25207d6b56ccab5cdafc2bd245e4
Bryce Klinker <bryce.klinker@gmail.com>
2017-12-07 22:31:12 -0600

    Start of legacy application

---
+++ dotnet/admin-dashboard/Admin.Web/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Admin.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ dotnet/admin-dashboard/Admin.Web/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+
+gulp.task('copy-fonts', function () {
+    return gulp.src([
+        './node_modules/bootstrap/dist/fonts/**/*.*'
+    ]).pipe(gulp.dest('./wwwroot/fonts'));
+});
+
+gulp.task('copy-styles', function ()  {
+    return gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        './node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+        './styles/styles.css'
+    ]).pipe(gulp.dest('./wwwroot/styles'));
+});
+
+gulp.task('copy-scripts', function ()  {
+    return gulp.src([
+        './node_modules/jquery/dist/jquery.min.js',
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/bootstrap/dist/js/bootstrap.min.js'
+    ]).pipe(gulp.dest('./wwwroot/scripts'));
+});
+
+gulp.task('default', ['copy-scripts', 'copy-styles', 'copy-fonts']);
